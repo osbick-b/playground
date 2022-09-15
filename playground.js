568baf01affa1fb171dc01a26e122f95804e1322
Lucien <osbick.b@gmail.com>
2022-09-15 16:53:24 +0200

    hamburger menu working with transition

---
+++ playground.js
@@ -0,0 +1,16 @@
+
+
+
+function toggleHamb() {
+    console.log("click");
+    const hambMenu = document.querySelector("#hamburger-menu");
+    console.log(`hambMenu`, hambMenu);
+    hambMenu.classList.toggle("off");
+    hambMenu.classList.toggle("on");
+}
+// const btnHambClose = hambMenu.querySelector("button.close");
+// console.log(`btnHambClose`, btnHambClose);
+// btnHambClose.addEventListener(onclick, (e) => {
+    
+//     console.log("click!");
+// });
